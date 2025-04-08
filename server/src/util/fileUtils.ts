8b67579fd10caf2293b86c7ec13ff10763e1eaab
Daniel Windham <daniel@atlascomputing.org>
2025-04-08 17:45:02 -0400

    fix: Clean up relative filepath loading

--- server/src/util/fileUtils.ts
+++ server/src/util/fileUtils.ts
@@ -5,3 +5,4 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const DATA_DIR = path.join(__dirname, '../../data');
+export const SRC_DIR = path.join(__dirname, '../../src');
