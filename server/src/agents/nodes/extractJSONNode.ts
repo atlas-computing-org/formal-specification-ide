be30e05528ad5077aa52eb4c37335020042e418b
Shaowei Lin <shaowei@gmail.com>
2025-04-11 21:28:14 -0700

    refactor: Use JSONOutputParser to extract JSON

--- server/src/agents/nodes/extractJSONNode.ts
+++ server/src/agents/nodes/extractJSONNode.ts
@@ -1,34 +1,13 @@
+import { JsonOutputParser } from "@langchain/core/output_parsers";
 import { StateInfo } from "../agent.ts";
 import { Logger } from "../../Logger.ts";
 import { responseContent } from "../agent.ts";
 
-const escapeString = (str: string) => {
-  return str.replace(/\\/g, '\\\\');
-}
+const parser = new JsonOutputParser();
 
-const extractJSON = (content: string, logger: Logger) => {
-  const jsonHeaderStart = content.indexOf('### JSON ANNOTATIONS');
-  const jsonStart = content.indexOf('```json', jsonHeaderStart) + 7;
-  const jsonEnd = content.indexOf('```', jsonStart);
-
-  // If the JSON block is incomplete, use the string up through the last complete object.
-  // Be sure to terminate the enclosing array.
-  const incompleteResponse = jsonEnd === -1;
-  const jsonString = incompleteResponse ?
-    content.slice(jsonStart, content.lastIndexOf('},\n') + 1).trim() + ']' :
-    content.slice(jsonStart, jsonEnd).trim();
-  logger.debug(`Extracted JSON string:\n${jsonString}`);
-
-  try {
-    return JSON.parse(escapeString(jsonString));
-  } catch (e) {
-    throw new Error(`Error parsing JSON in LLM's reponse: ${e}`);
-  }
-};
-
-export const extractJSONNode = (state: typeof StateInfo.State) => {
-  const resContent = responseContent(state); 
-  const outputAnnotations = extractJSON(resContent, state.logger);
+export const extractJSONNode = async (state: typeof StateInfo.State) => {
+  const response = state.messages[state.messages.length - 1];
+  const outputAnnotations =  await parser.invoke(response);
   state.logger.info("Successfully parsed JSON annotations from LLM's response.");
   state.logger.debug(`Parsed annotations:\n${JSON.stringify(outputAnnotations, null, 2)}`);
   return { outputAnnotations: outputAnnotations };
