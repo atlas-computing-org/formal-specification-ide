20a7c39b04d471d8b28c6dee71ded5f71b586406
Shaowei Lin <shaowei@gmail.com>
2025-04-21 13:09:07 -0700

    fix: Change verbose logs from info to debug

--- server/src/agents/nodes/annotateNode.ts
+++ server/src/agents/nodes/annotateNode.ts
@@ -5,8 +5,8 @@ import { StateInfo } from "../agent.ts";
 
 export const annotateNode = async (state: typeof StateInfo.State) => {
   const PROMPT = await fs.readFile(`${SERVER_SRC_DIR}/agents/nodes/annotateNodePromptSnippets.txt`, 'utf-8');
-  state.logger.info(`System Message:\n${PROMPT}`);
-  state.logger.info(`Human Message:\n${state.systemData}`);
+  state.logger.debug(`System Message:\n${PROMPT}`);
+  state.logger.debug(`Human Message:\n${state.systemData}`);
   return { messages: [
     new SystemMessage(PROMPT),
     new HumanMessage(state.systemData)
--- server/src/agents/nodes/chatNode.ts
+++ server/src/agents/nodes/chatNode.ts
@@ -5,6 +5,6 @@ import { StateInfo } from "../agent.ts";
 
 export const chatNode = async (state: typeof StateInfo.State) => {
   const PROMPT = await fs.readFile(`${SERVER_SRC_DIR}/agents/nodes/chatNodePrompt.txt`, 'utf-8');
-  state.logger.info(`System Message:\n${PROMPT + state.systemData}`);
+  state.logger.debug(`System Message:\n${PROMPT + state.systemData}`);
   return { messages: [new SystemMessage(PROMPT + state.systemData)] };
 }
