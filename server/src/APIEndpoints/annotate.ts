a7da5db847d146ae6414b8a79b765e6c6d69eac7
Shaowei Lin <shaowei@gmail.com>
2025-04-21 12:52:54 -0700

    refactor: Give specific names to graphs

--- server/src/APIEndpoints/annotate.ts
+++ server/src/APIEndpoints/annotate.ts
@@ -1,11 +1,11 @@
 import { Annotations } from "@common/annotations.ts";
 import { Logger } from '../Logger.ts';
-import { graph } from "../agents/graphs/annotateGraph.ts";
+import { annotateGraph } from "../agents/graphs/annotateGraph.ts";
 
 export const annotate = async (userUUID: string, lhsText: string, rhsText: string, currentAnnotations: Annotations,
     useDemoCache: boolean, logger: Logger) => {
 
   const config = { configurable: { thread_id: userUUID } };
-  const output = await graph.invoke({ lhsText: lhsText, rhsText: rhsText, currentAnnotations: currentAnnotations, useDemoCache: useDemoCache, logger: logger }, config);
+  const output = await annotateGraph.invoke({ lhsText: lhsText, rhsText: rhsText, currentAnnotations: currentAnnotations, useDemoCache: useDemoCache, logger: logger }, config);
   return output.decodedAnnotations;
 };
--- server/src/APIEndpoints/chat.ts
+++ server/src/APIEndpoints/chat.ts
@@ -1,12 +1,10 @@
 import { Annotations } from "@common/annotations.ts";
 import { Logger } from "../Logger.ts";
-import { graph } from "../agents/graphs/chatGraph.ts";
+import { chatGraph } from "../agents/graphs/chatGraph.ts";
 
 export const chatWithAssistant = async (userUUID: string, userInput: string, lhsText: string, rhsText: string, 
     currentAnnotations: Annotations, resetChat: boolean, logger: Logger) => {
 
   const config = { configurable: { thread_id: userUUID } };
-  const output = await graph.invoke({ userInput: userInput, lhsText: lhsText, rhsText: rhsText, currentAnnotations: currentAnnotations, resetChat: resetChat, logger: logger }, config);
-  const response = output.messages[output.messages.length - 1]
-  return response.content as string;
+  const output = await chatGraph.invoke({ userInput: userInput, lhsText: lhsText, rhsText: rhsText, currentAnnotations: currentAnnotations, resetChat: resetChat, logger: logger }, config);
 }
