f00052e776c2c3595f7b6c44876552a1d7e9bacd
Shaowei Lin <shaowei@gmail.com>
2025-04-21 13:09:07 -0700

    fix: Change verbose logs from info to debug

--- server/src/agents/nodes/userNode.ts
+++ server/src/agents/nodes/userNode.ts
@@ -2,6 +2,6 @@ import { HumanMessage } from "@langchain/core/messages";
 import { StateInfo } from "../agent.ts";
 
 export const userNode = (state: typeof StateInfo.State) => {
-  state.logger.info(`User Message:\n${state.userInput}`);
+  state.logger.debug(`User Message:\n${state.userInput}`);
   return { messages: [new HumanMessage(state.userInput)] };
 }
