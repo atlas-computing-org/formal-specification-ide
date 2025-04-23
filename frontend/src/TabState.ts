82981f4fe3d5c9693d0445e15b820f6fae6d109d
Daniel Windham <daniel@atlascomputing.org>
2025-04-23 16:13:52 -0400

    refactor: React migration: Delete old files

--- frontend/src/TabState.ts
+++
@@ -1,8 +0,0 @@
-export type LeftTabMode = "pdf" | "full-text" | "selected-text"
-
-export type RightTabMode = "pre-written" | "generated"
-
-export interface TabState {
-  left: LeftTabMode;
-  right: RightTabMode;
-}
