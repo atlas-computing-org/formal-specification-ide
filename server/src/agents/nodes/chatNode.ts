3af54f6d056f3fc562d5ed8289c52995604902f2
Shaowei Lin <shaowei@gmail.com>
2025-04-25 15:37:22 -0700

    fix: Clean up relative filepath loading

--- server/src/agents/nodes/chatNode.ts
+++ server/src/agents/nodes/chatNode.ts
@@ -1,9 +1,10 @@
 import { promises as fs } from 'node:fs';
 import { SystemMessage } from "@langchain/core/messages";
+import { SERVER_SRC_DIR } from '../../util/fileUtils.ts';
 import { StateInfo } from "../agent.ts";
 
 export const chatNode = async (state: typeof StateInfo.State) => {
-  const PROMPT = await fs.readFile('./chatNodePrompt.txt', 'utf-8');
+  const PROMPT = await fs.readFile(`${SERVER_SRC_DIR}/agents/nodes/chatNodePrompt.txt`, 'utf-8');
   state.logger.info(`System Message:\n${PROMPT + state.systemData}`);
   return { messages: [new SystemMessage(PROMPT + state.systemData)] };
 }
