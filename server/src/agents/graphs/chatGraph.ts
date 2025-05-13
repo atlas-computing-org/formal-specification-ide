d5ea6b69342b19fcfb8445fd1fc117470078742f
Shaowei Lin <shaowei@gmail.com>
2025-05-13 16:03:32 -0700

    fix: Remove chatReset and use sessionId instead

--- server/src/agents/graphs/chatGraph.ts
+++ server/src/agents/graphs/chatGraph.ts
@@ -2,29 +2,27 @@ import { START, END, StateGraph, MemorySaver } from "@langchain/langgraph";
 import { Annotations } from "@common/annotations.ts";
 import { Logger } from "../../Logger.ts";
 import { GraphError, StateInfo, responseContent } from '../agent.ts';
-import { chatNode } from "../nodes/chatNode.ts";
+import { initChatNode } from "../nodes/initChatNode.ts";
 import { encodeAnnotationsNode } from "../nodes/encodeAnnotationsNode.ts";
 import { modelNode } from "../nodes/modelNode.ts";
 import { userNode } from "../nodes/userNode.ts";
 import { ChatAboutAnnotationsResponse } from "@common/serverAPI/chatAboutAnnotationsAPI.ts";
 
-// Slight hack for now - decide if the chat is reset by checking if the sessionId is new
-let lastSessionId: string | null = null;
-
 // Add conditional edge for resetting chat
-const resetChoice = (state: typeof StateInfo.State) => {
-  return state.chatReset ? "encodeAnnotations" : "user";
+const initChoice = (state: typeof StateInfo.State) => {
+  state.logger.info(`initChoice: state.messages.length ${state.messages.length}`);
+  return (state.messages.length === 0) ? "encodeAnnotations" : "user";
 }
 
 // Define a new graph
 const workflow = new StateGraph(StateInfo)
   .addNode("encodeAnnotations", encodeAnnotationsNode)
-  .addNode("chat", chatNode)
+  .addNode("initChat", initChatNode)
   .addNode("user", userNode)
   .addNode("model", modelNode)
-  .addConditionalEdges(START, resetChoice)
-  .addEdge("encodeAnnotations","chat")
-  .addEdge("chat", "user")
+  .addConditionalEdges(START, initChoice)
+  .addEdge("encodeAnnotations","initChat")
+  .addEdge("initChat", "user")
   .addEdge("user", "model")
   .addEdge("model", END);
 
@@ -35,15 +33,10 @@ const memory = new MemorySaver();
 export const chatGraph = workflow.compile({ checkpointer: memory });
 
 export async function chatGraphInvoke(userInput: string, lhsText: string, rhsText: string, oldAnnotations: Annotations, sessionId: string, logger: Logger): Promise<ChatAboutAnnotationsResponse> {
-  // TODO - cleanup chat reset logic in the graph
-  let chatReset = false;
-  if (sessionId !== lastSessionId) {
-    lastSessionId = sessionId;
-    chatReset = true;
-  }
   try {
     const config = { configurable: { thread_id: sessionId } };
-    const rawOutput = await chatGraph.invoke({ userInput, lhsText, rhsText, oldAnnotations, chatReset, logger }, config);
+    logger.info(`chatGraphInvoke: sessionId ${sessionId}`);
+    const rawOutput = await chatGraph.invoke({ userInput, lhsText, rhsText, oldAnnotations, logger }, config);
     return { data: responseContent(rawOutput) };
   } catch (e) {
     if (e instanceof GraphError) {
